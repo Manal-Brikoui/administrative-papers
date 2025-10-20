cbfc5a4f41b5650dead8520b1fde1a3138363683
Manal Brikoui <manal.brikoui.ensao@ump.ac.ma>
2025-10-20 22:33:23 +0100

    Update DocumentType.cs

--- Backend/CitizenServer.Domain/Entities/DocumentType.cs
+++ Backend/CitizenServer.Domain/Entities/DocumentType.cs
@@ -10,22 +10,22 @@ namespace CitizenServer.Domain.Entities
         [Key]
         public Guid Id { get; set; }  // Identifiant unique du document type
 
-        public required string Name { get; set; }  // Nom du type de document (ex: "Pièce d'identité")
+        public required string Name { get; set; }  // Nom du type de document 
 
         public bool IsImportable { get; set; }  // Indique si le document est importable
 
-        public string Category { get; set; }  // Catégorie du document (ex: "Identité")
+        public string Category { get; set; }  // Catégorie du document 
 
-        // Clé étrangère vers Category
+      
         public Guid CategoryId { get; set; }
 
-        [JsonIgnore]  // Évite le cycle infini lors de la sérialisation
+        [JsonIgnore]  
         public Category CategoryEntity { get; set; }
 
-        // Clé étrangère vers TypeDossier
+       
         public Guid TypeDossierId { get; set; }
 
-        [JsonIgnore]  // Évite le cycle infini lors de la sérialisation
+        [JsonIgnore]  
         public TypeDossier TypeDossierEntity { get; set; }
 
         public DocumentType() { }
