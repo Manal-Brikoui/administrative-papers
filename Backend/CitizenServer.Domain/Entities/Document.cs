e785d2e3a88d01bdb31dbbcec7b54e2808293111
Manal Brikoui <manal.brikoui.ensao@ump.ac.ma>
2025-10-20 22:32:39 +0100

    Update Document.cs

--- Backend/CitizenServer.Domain/Entities/Document.cs
+++ Backend/CitizenServer.Domain/Entities/Document.cs
@@ -12,14 +12,14 @@ namespace CitizenServer.Domain.Entities
         [Key]
         public Guid Id { get; set; }  // Identifiant unique du document
         public Guid UserId { get; set; }
-        public Guid DossierAdministratifId { get; set; }  // Clé étrangère vers DossierAdministratif
-        public required string Type { get; set; }  // Type du document (par exemple : "Pièce d'identité", "Justificatif de domicile")
+        public Guid DossierAdministratifId { get; set; }  
+        public required string Type { get; set; }  // Type du document 
         public required string FilePath { get; set; }  // Chemin d'accès du fichier document
         public DateTime UploadDate { get; set; }  // Date de soumission du document
         public bool IsOnPlatform { get; set; }  // Indique si le document est présent sur la plateforme
         public string? ImportLocation { get; set; }  // L'emplacement d'importation du document
 
-        // Référence vers DossierAdministratif
+   
         public DossierAdministratif DossierAdministratif { get; set; }
     }
 }
