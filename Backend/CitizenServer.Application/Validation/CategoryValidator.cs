fdae15c9e0dc76f118a142a1c94b0f5095d11c19
Manal Brikoui <manal.brikoui.ensao@ump.ac.ma>
2025-10-20 11:16:52 +0100

    Update CategoryValidator.cs

--- Backend/CitizenServer.Application/Validation/CategoryValidator.cs
+++ Backend/CitizenServer.Application/Validation/CategoryValidator.cs
@@ -7,18 +7,16 @@ namespace CitizenServer.Validation
     {
         public CategoryValidator()
         {
-            // Vérification du nom (obligatoire et longueur max)
+            // Vérification du nom 
             RuleFor(c => c.Name)
                 .NotEmpty().WithMessage("Le nom de la catégorie est obligatoire.")
                 .MaximumLength(100).WithMessage("Le nom de la catégorie ne doit pas dépasser 100 caractères.");
 
-            // Vérification de la description (facultative, mais longueur max si remplie)
+            // Vérification de la description 
             RuleFor(c => c.Description)
                 .MaximumLength(500).WithMessage("La description ne doit pas dépasser 500 caractères.");
 
-            // Vérifier qu'il y a au moins un type de document associé si nécessaire
-            // RuleFor(c => c.DocumentTypes)
-            //     .NotEmpty().WithMessage("La catégorie doit contenir au moins un type de document.");
+            
         }
     }
 }
