74ddccd5a7df4c58d09651a8a83539e3f81d8ea6
Manal Brikoui <manal.brikoui.ensao@ump.ac.ma>
2025-10-20 22:34:53 +0100

    Update Notification.cs

--- Backend/CitizenServer.Domain/Entities/Notification.cs
+++ Backend/CitizenServer.Domain/Entities/Notification.cs
@@ -14,7 +14,7 @@ namespace CitizenServer.Domain.Entities
         public Guid UserId { get; set; }
 
         [Required]
-        public string Type { get; set; }  // Type de notification (par exemple : "Rappel", "Alert", "Info")
+        public string Type { get; set; }  // Type de notification
 
         [Required]
         public string Message { get; set; }  // Le message de la notification
@@ -22,17 +22,18 @@ namespace CitizenServer.Domain.Entities
         public DateTime NotificationDate { get; set; }  // Date et heure de la notification
 
         [Required]
-        public string Status { get; set; }  // Statut de la notification (par exemple : "Non lu", "Lu")
+        public string Status { get; set; }  // Statut de la notification
+
 
         [Required]
-        public string Channel { get; set; }  // Canal de la notification (par exemple : "Email", "SMS")
+        public string Channel { get; set; }  // Canal de la notification
 
         public bool IsRead { get; set; }  // Indique si la notification a été lue ou non
 
-        public Guid? RelatedEntityId { get; set; }  // Référence à l'entité liée à la notification (Dossier, Rendezvous, etc.)
+        public Guid? RelatedEntityId { get; set; }  // Référence à l'entité liée à la notification
 
         [Required]
-        public string RelatedEntityType { get; set; }  // Type de l'entité liée (par exemple : "Dossier", "Rendezvous")
+        public string RelatedEntityType { get; set; }  // Type de l'entité liée 
 
         public Notification()
         {
